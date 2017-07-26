1f5f3edbf1e01f91ca53a75fc67c8b7abe23957a
misturfixit <japullins420@gmail.com>
2017-07-26 01:47:52 -0400

    summit

---
+++ app0.rb
@@ -0,0 +1,5 @@
+require "sinatra"
+enable :sessions
+	get '/' do 
+	erb :mo
+end
---
+++ visages/moniker.rb
@@ -0,0 +1,5 @@
+<h1> Provideth thy title </h1>
+<form method = "post" action = "name">
+ 	<input type = "text" name = title> 
+ 	<input type = "submit" value = "THISISABUTTON">
+ </form>		
