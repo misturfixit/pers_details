0173c91dcc61a1a81b1611b28ae019f3425de086
misturfixit <japullins420@gmail.com>
2017-07-27 03:34:22 -0400

    oomgawnotbeenpushn

--- app0.rb
+++ app0.rb
@@ -1,5 +1,60 @@
 require "sinatra"
 enable :sessions
-	get '/' do 
-	erb :mo
+get '/' do 
+	msg = params[:msg] || ""
+	erb :login, locals: {msg: msg}
+end	
+
+def login(username, password)
+	if username == "Joe" && password == "Bro"
+		redirect '/name'	
+	else
+		redirect '/?msg=INCORRECT USERNAME/PASSWORD..
+		...3 TRYS REMAIN UNTIL SELF-DESTRUCT'
+	end	
 end
+
+
+post '/username' do
+	username = params[:username]
+	password = params[:password]
+	login(username, password) 
+end
+
+get '/name' do
+ 	frstn = params[:title]
+	erb :moniker, locals: {frstn: frstn}
+end
+
+post '/name' do
+	#session[:age] = 38
+ 	frstn = params[:title]
+ 	redirect '/surname?frstn=' + frstn
+end
+
+get '/surname' do
+	frstn = params[:frstn]	
+	erb :sur, locals: {frstn: frstn}
+end
+ 
+post '/surname' do
+	s_name = params[:s_name]
+	frstn = params[:frstn]
+	redirect '/visage?s_name=' + s_name + "&frstn=" + frstn 
+end	
+
+get '/visage' do
+	frstn = params[:frstn]
+	s_name = params[:surname]
+	erb :visage, locals: {frstn: frstn, s_name: s_name} 
+end
+
+
+post '/visage' do
+	fur = params[:fur]
+ 	orbs = params[:orbs]
+ 	fname = params[:frstn]
+ 	surname = params[:s_name]
+ 	redirect '/numb?vis=' + s_name + "&frstn=" + frstn 
+end
+ 
