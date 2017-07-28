33d62190334a7b96fc33652999a950aa3a6e33f8
misturfixit <japullins420@gmail.com>
2017-07-28 14:28:42 -0400

    wowsers

--- app0.rb
+++ app0.rb
@@ -25,35 +25,51 @@ get '/name' do
  	frstn = params[:title]
 	erb :moniker, locals: {frstn: frstn}
 end
-
 post '/name' do
 	#session[:age] = 38
  	frstn = params[:title]
  	redirect '/surname?frstn=' + frstn
 end
-
 get '/surname' do
 	frstn = params[:frstn]	
 	erb :sur, locals: {frstn: frstn}
 end
- 
 post '/surname' do
 	s_name = params[:s_name]
 	frstn = params[:frstn]
-	redirect '/fur?s_name=' + s_name + "&frstn=" + frstn 
+	redirect '/furry?s_name=' + s_name + "&frstn=" + frstn 
 end	
-
-get '/fur' do
+get '/furry' do
 	frstn = params[:frstn]
 	s_name = params[:s_name]
 	erb :visage, locals: {frstn: frstn, s_name: s_name} 
 end
-
-
-post '/fur' do
+post '/furry' do
 	fur = params[:fur]
- 	fname = params[:frstn]
+ 	frstn = params[:frstn]
  	s_name = params[:s_name]
- 	redirect '/numb?vis=' + s_name + "&frstn=" + frstn + "&fur"
+ 	redirect '/balls?fur=' + fur + "&s_name=" + s_name + "&frstn=" + frstn
 end
- 
+get '/balls' do
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	erb :vis, locals: {frstn: frstn, s_name: s_name, fur: fur} 
+end
+post '/balls' do
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	redirect '/age?orbs=' + orbs + "&fur=" + fur + "&s_name=" + s_name + "&frstn=" + frstn 
+end
+get '/age' do 
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	erb :years, locals: {frstn: frstn, s_name: s_name, fur: fur, orbs: orbs} 
+end
+
+
+
