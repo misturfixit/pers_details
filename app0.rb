7ec3c9e875f7eeb6f28661e182d1c9a20eb9c41b
misturfixit <japullins420@gmail.com>
2017-07-29 23:22:59 -0400

    whooboi

--- app0.rb
+++ app0.rb
@@ -70,6 +70,83 @@ get '/age' do
  	s_name = params[:s_name]
  	erb :years, locals: {frstn: frstn, s_name: s_name, fur: fur, orbs: orbs} 
 end
+post '/age' do
+	yrs = params[:yrs]
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	redirect '/food&?yrs=' + yrs  + "&orbs=" + orbs + "&fur=" + fur + "&s_name=" + s_name + "&frstn=" + frstn 
+end
+
+get '/food&' do
+	yrs = params[:yrs]
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	erb :fare, locals: {frstn: frstn, s_name: s_name, fur: fur, orbs: orbs, yrs: yrs} 
+end
+
+post '/food&' do
+	grub = params[:grub]
+	libs = params[:libs]  
+	yrs = params[:yrs]
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	redirect '/numbers?grub=' + grub + "&libs=" + libs + "&yrs=" + yrs  + "&orbs=" + orbs + "&fur=" + fur + "&s_name=" + s_name + "&frstn=" + frstn 
+end
+
+
+get '/numbers' do 
+	grub = params[:grub]
+	libs = params[:libs] 
+	yrs = params[:yrs]
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	erb :numb, locals: {frstn: frstn, s_name: s_name, fur: fur, orbs: orbs, yrs: yrs, grub: grub, libs: libs} 
+end
+
+
+post '/numbers' do
+	 n1 = params[:n1]
+	 n2 = params[:n2]
+	 n3 = params[:n3]
+# def consilidation
+# 	nums = [:n1, :n2, :n3]
+#    nums = params[:nums]
+# end
+
+	#nums = params[:nums]
+	grub = params[:grub]
+	libs = params[:libs] 
+   yrs = params[:yrs]
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+ 	redirect '/res?n1=' + n1 + "&n2=" n2 + "&n3=" + n3 + "&grub=" + grub + "&libs=" + libs + "&yrs=" + yrs + "&orbs=" + orbs + "&fur=" + fur + "&s_name=" + s_name + "&frstn=" + frstn 
+end
+
+get '/res' do
+	#nums = params[:nums]
+	n1 = params[:n1]
+   n2 = params[:n2]
+	n3 = params[:n3]
+	grub = params[:grub]
+	libs = params[:libs] 
+   yrs = params[:yrs]
+	orbs = params[:orbs]
+	fur = params[:fur]
+ 	frstn = params[:frstn]
+ 	s_name = params[:s_name]
+	erb :results, locals: {frstn: frstn, s_name: s_name, fur: fur, orbs: orbs, yrs: yrs, grub: grub, libs: libs, n1: n1, n2: n2, n3: n3} 
+end 	
 
 
 
+#n1= n1 + "&n2=" + n2 + "&n3=" + n3 +
